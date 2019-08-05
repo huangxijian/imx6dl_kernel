66f7b61c5240c7635e86bf92e51595a1f64055d7
huangxijian <huangxijian88@gmail.com>
2019-08-05 22:59:28 +0800

    delete null line

--- zImage.sh
+++ zImage.sh
@@ -1,7 +1,6 @@
 #! /bin/sh
 
 #source /opt/fsl-imx-x11/3.14.52-1.1.0/environment-setup-cortexa9hf-vfp-neon-poky-linux-gnueabi 
-
 export ARCH=arm
 export CROSS_COMPILE=$TARGET_PREFIX
 #unset LDFLAGS
